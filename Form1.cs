93cdc1ca562e821725de51c9e38141040ac8435a
Guilherme Oliveira <guilhermeoliveira36023@gmail.com>
2024-01-19 01:08:57 -0300

    First commit

--- Form1.cs
+++ Form1.cs
@@ -2,12 +2,23 @@ using System.IO;
 
 namespace Station_Cycle_time_analyzer
 {
+
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+
+        public partial class Product
+        {
+            public string blockNumber = string.Empty;
+            public DateTime signInTimeStamp;
+            public DateTime signOutTimeStamp;
+        }
+
+        List<Product> productList = new List<Product>();
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -20,10 +31,23 @@ namespace Station_Cycle_time_analyzer
 
                 textBox1.Text = line;
 
-                while (line != null) 
+                while (line != null)
                 {
-                    Console.WriteLine(line);
                     line = sr.ReadLine();
+                    if (line.Contains("Barcode step 4/4"))
+                    {
+                        Product product = new Product();
+                        
+                        product.signInTimeStamp = DateTime.Parse(line.Substring(0, 13));
+                        textBox1.Text = product.signInTimeStamp.ToLongTimeString();
+
+                        product.blockNumber = line.Substring(line.IndexOf(",\"Barcode\":\"B"), 25);
+                        textBox1.Text = product.blockNumber;
+                        textBox1.Text = product.blockNumber;
+
+
+                        //productList.Add()
+                    }
                 }
 
                 sr.Close();
